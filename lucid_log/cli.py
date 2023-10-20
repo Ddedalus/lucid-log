eb1ddb3330dfc59afc9e1fca858b975e80f1935c
Hubert Bereś <hubert@springbok.ai>
2023-10-20 14:11:59 +0100

    Basic log printer

--- lucid_log/cli.py
+++ lucid_log/cli.py
@@ -1,3 +1,7 @@
+import json
+from pathlib import Path
+from typing import Annotated, Optional
+from rich import print_json
 import typer
 
 app = typer.Typer()
@@ -9,5 +13,21 @@ def help():
 
 
 @app.command()
-def show():
-    print("Hello!")
+def show(log_file: Annotated[Optional[Path], typer.Argument()] = None):
+    if log_file is None:
+        typer.secho("Parsing logs from standard input...", fg=typer.colors.GREEN)
+        while True:
+            line = input()
+            process_line(line)
+
+    with log_file.open() as file:
+        for line in file.readlines():
+            process_line(line)
+
+
+def process_line(line: str):
+    try:
+        data = json.loads(line)
+        print_json(data=data)
+    except json.JSONDecodeError:
+        typer.secho(f"> {line}", fg=typer.colors.BRIGHT_BLACK)
