c5fa8e8ec608166f7431a1634a5ad0104d13a458
Hubert Bereś <hubert@springbok.ai>
2023-10-20 14:18:19 +0100

    Formatting

--- lucid_log/cli.py
+++ lucid_log/cli.py
@@ -1,8 +1,9 @@
 import json
 from pathlib import Path
 from typing import Annotated, Optional
-from rich import print_json
+
 import typer
+from rich import print_json
 
 app = typer.Typer()
 
@@ -18,14 +19,14 @@ def show(log_file: Annotated[Optional[Path], typer.Argument()] = None):
         typer.secho("Parsing logs from standard input...", fg=typer.colors.GREEN)
         while True:
             line = input()
-            process_line(line)
+            display_line(line)
 
     with log_file.open() as file:
         for line in file.readlines():
-            process_line(line)
+            display_line(line)
 
 
-def process_line(line: str):
+def display_line(line: str):
     try:
         data = json.loads(line)
         print_json(data=data)
